21c67c71c5cd443b363b553ef433ac8e5afdcfdd
li1ht2ay <108039498+li1ht2ay@users.noreply.github.com>
2023-03-11 19:11:08 -0500

    Update libretro.cpp

--- libretro/libretro.cpp
+++ libretro/libretro.cpp
@@ -1304,7 +1304,7 @@ void retro_run(void)
    int dif = blargg_ntsc ? 9 : 4;
 
    // Absolute mess of inline if statements...
-   video_cb(video_buffer + (overscan_v_top ? ((((overscan_h_left + overscan_h_right) * dif) / 4) + (blargg_ntsc ? Api::Video::Output::NTSC_WIDTH : Api::Video::Output::WIDTH) * 8) : ((overscan_h_left * dif) / 4) + 0)
+   video_cb(video_buffer + (overscan_v_top ? ((((overscan_h_left + overscan_h_right) * dif) / 4) + (blargg_ntsc ? Api::Video::Output::NTSC_WIDTH : Api::Video::Output::WIDTH) * 8) : ((overscan_h_left * dif) / 4) + 0),
          video_width - (((overscan_h_left + overscan_h_right) * dif) / 4),
          Api::Video::Output::HEIGHT - (overscan_v_top + overscan_v_bottom),
          pitch);
